c6c0b444e6a073bd87b4a0e13e49e77ff7e8f61f
Tony Eder <tonyeder11@gmail.com>
2024-03-02 21:18:52 -0800

    static version of capture catalog completed. next to work on fetch, update, delete, etc with db.json file

--- src/Catalog.js
+++ src/Catalog.js
@@ -1,33 +1,49 @@
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import './App.css';
-
+import Row from 'react-bootstrap/Row'
 
 const Catalog = () => {
     return (
         <>
-            <Col className="" md="4">
-                <div className="test-box">Where is this?</div>
-                <p>Name: Tony Eder</p>
-                <p>School: Purdue</p>
-                <p>Location: Indiana</p>
-                <Button onClick="" type="submit" value="Submit">Reset Form</Button>
+            <Col md="4">
+                <Row>
+                    <Col className="" md="4">
+                        <div className="test-box"></div>
+                    </Col>
+                    <Col md="8">
+                        <p>Name: Tony Eder</p>
+                        <p>School: Purdue</p>
+                        <p>Location: Indiana</p>
+                        <Button onClick="" type="submit" value="Submit">Reset Form</Button>
+                    </Col>
+                </Row>
             </Col>
-
-            <Col className="" md="4">
-                <div className="test-box"></div>
-                <p>Name: Tony Eder</p>
-                <p>School: Purdue</p>
-                <p>Location: Indiana</p>
-                <Button onClick="" type="submit" value="Submit">Reset Form</Button>
+            <Col md="4">
+                <Row>
+                    <Col className="" md="4">
+                        <div className="test-box"></div>
+                    </Col>
+                    <Col md="8">
+                        <p>Name: Tony Eder</p>
+                        <p>School: Purdue</p>
+                        <p>Location: Indiana</p>
+                        <Button onClick="" type="submit" value="Submit">Reset Form</Button>
+                    </Col>
+                </Row>
             </Col>
-
-            <Col className="" md="4">
-                <div className="test-box"></div>
-                <p>Name: Tony Eder</p>
-                <p>School: Purdue</p>
-                <p>Location: Indiana</p>
-                <Button onClick="" type="submit" value="Submit">Reset Form</Button>
+            <Col md="4">
+                <Row>
+                    <Col className="" md="4">
+                        <div className="test-box"></div>
+                    </Col>
+                    <Col md="8">
+                        <p>Name: Tony Eder</p>
+                        <p>School: Purdue</p>
+                        <p>Location: Indiana</p>
+                        <Button onClick="" type="submit" value="Submit">Reset Form</Button>
+                    </Col>
+                </Row>
             </Col>
         </>
     )
